db7192d38aba1760759898fba3d1a6dcdc56d2ab
malinhodev <malinhodev@gmail.com>
2025-07-08 00:51:13 -0300

    extrurando projeto e iniciando layout

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
-console.log("working...")
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    duration: 1200,
+    reset: true
+  })
+  
+  scrollReveal.reveal(
+    `.topo
+    `,
+    { interval: 200 }
+  )
+
+  const scrollRevealLeft = ScrollReveal({
+    origin: 'left',
+    distance: '35px',
+    duration: 1200,
+    reset: true
+  })
+  
+  scrollRevealLeft.reveal(
+    `.left
+    `,
+    { interval: 200 }
+  )
+
+  const scrollRevealRight = ScrollReveal({
+    origin: 'right',
+    distance: '35px',
+    duration: 1200,
+    reset: true
+  })
+  
+  scrollRevealRight.reveal(
+    `.right
+    `,
+    { interval: 200 }
+  )
